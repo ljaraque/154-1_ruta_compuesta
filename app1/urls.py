a7b0095c79bb500ee63c64d621351c6bbdfe4db2
ljaraque <ljaraque@yahoo.com>
2020-12-28 11:42:40 -0300

    m5-clase1-c

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.concinero_inicio),
+    path('inicio', views.index),
+    path('', views.principal),
 ]
--- app1/views.py
+++ app1/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 import datetime
 import random
 
-def concinero_inicio(request):
+def index(request):
 
     fecha_hora_actual = datetime.datetime.now()
     data = {'contenido_dinamico': str(fecha_hora_actual),
             'contenido_dinamico2': [i for i in range(0,random.randint(2,10))]}
-    return render(request, 'app1/hamburguesa.html', context = data)
+    return render(request, 'app1/index.html', context = data)
+
+
+def principal(request):
+    return render(request, 'app1/principal.html')
