266047406bb70494e2152372456cad321a8dc974
ljaraque <ljaraque@yahoo.com>
2020-12-28 10:01:33 -0300

    últimos ajustes

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+import datetime
+import random
 
 def concinero_inicio(request):
-    return render(request, 'app1/hamburguesa.html')
+
+    fecha_hora_actual = datetime.datetime.now()
+    data = {'contenido_dinamico': str(fecha_hora_actual),
+            'contenido_dinamico2': [i for i in range(0,random.randint(2,10))]}
+    return render(request, 'app1/hamburguesa.html', context = data)
