eac9ca0e48c009f53b7d3382c87fdbe7133d38c1
ljaraque <ljaraque@yahoo.com>
2021-01-19 11:50:29 -0300

    clase 19/01/2021 Relaciones Muchos a Muchos

--- formularios/models.py
+++ formularios/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Musico(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    banda = models.CharField(max_length=50)
+    pais =  models.CharField(max_length=50)
+
+
 class Guitarra(models.Model):
     marca = models.CharField(max_length=100)
     modelo = models.CharField(max_length=50)
     cuerdas = models.IntegerField()
+    madera = models.CharField(max_length=50, default="No definida")
     fecha_compra = models.DateField()
+    musico = models.ForeignKey(Musico, on_delete = models.CASCADE)
+
