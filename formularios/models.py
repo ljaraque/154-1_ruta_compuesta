296ed674d1401c6540a7440314295f2bd01acb77
nikonan <nik.31@hotmail.com>
2021-01-20 11:55:34 -0300

    clase 4 modulo 6 Migraciones

--- formularios/models.py
+++ formularios/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
 class Musico(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    banda = models.CharField(max_length=50)
     pais =  models.CharField(max_length=50)
+
+
+class Banda(models.Model):
+    nombre = models.CharField(max_length=50)
+    fecha_formacion = models.DateField()
+    musicos = models.ManyToManyField(Musico,related_name='bandas')
 
 
 class Guitarra(models.Model):
